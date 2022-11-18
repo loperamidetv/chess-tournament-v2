217641beaa09bbb18b5d830e6f4b42ff64f86fc5
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-18 20:41:04 +0400

    create the file

---
+++ mvc/models/crud.py
@@ -0,0 +1,17 @@
+class Crud:
+
+    def __init__(self, TABLE):
+        self.TABLE = TABLE
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
