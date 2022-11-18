bbc26ca1d6404d96490d5f9387354858fab47c83
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-18 20:32:46 +0400

    Create the pgrm entry point and test BS

---
+++ main.py
@@ -0,0 +1,13 @@
+from mvc.models.tournament import Tournament
+
+def run() -> None:
+    print(Tournament(
+        name='123', 
+        location='123'
+    ))
+
+
+
+
+if (__name__ == "__main__"):
+    run()
