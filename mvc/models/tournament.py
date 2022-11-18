f64425fac0880abf65b33595b7f321f663c19e23
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-18 13:19:40 +0400

    create proto tournament  model

---
+++ mvc/models/tournament.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from datetime import date
+
+@dataclass 
+class Tournament: 
+    '''
+    Notice
+
+    '''
+
+    name: str
+    location: str
+    date_start: date
+    date_end: date
+    nb_rounds: int
+    rounds: list
+    players: list
+    time_mode: str
+    description: str 
+
+    
