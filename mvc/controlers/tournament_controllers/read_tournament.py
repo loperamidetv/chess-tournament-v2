dc72c0820d4c374745eb1341d76c2a6773240d3a
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-19 20:49:00 +0400

    fully functional, will work when model will be ok

---
+++ mvc/controlers/tournament_controllers/read_tournament.py
@@ -0,0 +1,21 @@
+from mvc.models.tournament import Tournament
+from cfg.dbconst import TOURNAMENT_TABLE
+
+def read_tournament(ID: int, tournament_instance: Tournament) -> None:
+    '''
+    Given the ID, the function aims to gather the data from the database 
+    and update the tournament instance attributes with the data
+    '''
+    tournament_db_data = TOURNAMENT_TABLE.get(doc_id=ID)
+
+    # Updating the object Tournament attributes
+    for key, value in tournament_db_data.items():
+        tournament_instance.__dict__[key] = value
+
+    # Updating the ID, because the ID doesn't appear in 
+    # the tournament data from DB
+    tournament_instance.ID = ID
+
+
+
+    return None
