a4baa86346e97e42d320fd4e0b1f778269e39716
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-19 19:59:43 +0400

    dataclass refact and simplification for testing

--- mvc/models/tournament.py
+++ mvc/models/tournament.py
@@ -1,21 +1,9 @@
 from dataclasses import dataclass
-from datetime import date
 
-@dataclass 
-class Tournament: 
-    '''
-    Notice
-
-    '''
+@dataclass
+class Tournament(object):
 
     name: str
     location: str
-    date_start: date
-    date_end: date
-    nb_rounds: int
-    rounds: list
-    players: list
-    time_mode: str
-    description: str 
-
-    # create the basic crud operations
+    ID: int = None
+    # W.I.P
