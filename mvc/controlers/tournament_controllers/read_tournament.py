985375f4389bb34c4e142a933bac01496558c6ce
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-20 11:53:18 +0400

    clean the useless blank lines and spaces

--- mvc/controlers/tournament_controllers/read_tournament.py
+++ mvc/controlers/tournament_controllers/read_tournament.py
@@ -1,12 +1,12 @@
 from mvc.models.tournament import Tournament
-from cfg.dbconst import TOURNAMENT_TABLE
+from cfg.dbconst import TOURNAMENTS_TABLE
 
 def read_tournament(ID: int, tournament_instance: Tournament) -> None:
     '''
     Given the ID, the function aims to gather the data from the database 
     and update the tournament instance attributes with the data
     '''
-    tournament_db_data = TOURNAMENT_TABLE.get(doc_id=ID)
+    tournament_db_data = TOURNAMENTS_TABLE.get(doc_id=ID)
 
     # Updating the object Tournament attributes
     for key, value in tournament_db_data.items():
@@ -15,7 +15,5 @@ def read_tournament(ID: int, tournament_instance: Tournament) -> None:
     # Updating the ID, because the ID doesn't appear in 
     # the tournament data from DB
     tournament_instance.ID = ID
-
-
 
     return None
