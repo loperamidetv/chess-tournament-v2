5b25a266fbe41d39db84adf9f3ef44cfce96102c
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-20 11:58:28 +0400

    now can create player and push it onto db

--- mvc/controlers/player_controllers/create_player.py
+++ mvc/controlers/player_controllers/create_player.py
@@ -1,4 +1,17 @@
 from mvc.models.player import Player
 from cfg.dbconst import PLAYERS_TABLE
 
-def create
+def create_player(player_instance: Player) -> None:
+    '''
+    We serialize the data and put it in the db. 
+    We pop the ID because we don't need it to push data into db
+
+    More info see create_tournament.py file in tournament model folder
+    '''
+
+    serialized_data = player_instance.__dict__
+    serialized_data.pop('ID')
+
+    PLAYERS_TABLE.insert(serialized_data)
+
+    return None
