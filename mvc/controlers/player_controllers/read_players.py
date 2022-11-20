3383ea79557dd4fcf46bf754b8354623a38b96d4
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-20 11:53:41 +0400

    can read data from db and use it

---
+++ mvc/controlers/player_controllers/read_players.py
@@ -0,0 +1,22 @@
+from mvc.models.player import Player
+from cfg.dbconst import PLAYERS_TABLE
+
+def read_players(ID: int, player_instance: Player) -> None:
+    '''
+    Given the ID, the function aims to gather the data from the database 
+    and update the player instance attributes with the data
+    '''
+
+    player_db_data = PLAYERS_TABLE.get(doc_id=ID)
+
+    # Updating the Player instance's attributes
+    for key, value in player_db_data.items():
+        player_instance.__dict__[key] = value
+
+    # Updating the ID, because the ID doesn't appear in 
+    # the tournament data from DB
+    player_instance.ID = ID
+
+
+    return None
+
