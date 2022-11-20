c1c1ce0ba3ee5018a595a589bf6790acf0c130f1
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-20 10:51:44 +0400

    players table const created

--- cfg/dbconst.py
+++ cfg/dbconst.py
@@ -8,4 +8,4 @@ from tinydb import TinyDB
 
 DATABASE_PATH = 'database.json'
 TOURNAMENTS_TABLE = TinyDB(DATABASE_PATH).table('tournaments')
-PLAYERS_TABLE = 
+PLAYERS_TABLE = TinyDB(DATABASE_PATH).table('players')
