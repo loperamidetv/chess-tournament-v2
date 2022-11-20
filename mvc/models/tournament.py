06095cf2539a3acca5398e4b3e25221e736ccdfe
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-20 12:33:27 +0400

    adding a round attribute

--- mvc/models/tournament.py
+++ mvc/models/tournament.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
-from datetime import date
+from datetime import datetime
 
 @dataclass
 class Tournament(object):
 
     name: str = None
     location: str = None
-    date_start: date = None
-    date_end: date = None
+    date_start: datetime = None
+    date_end: datetime = None
     number_of_rounds: int = None
     rounds: list = None
     players: list = None
     time_mode: str = None
+    rounds: list = None 
     description: str = None
     ID: int = None
