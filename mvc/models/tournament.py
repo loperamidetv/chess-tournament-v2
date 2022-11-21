1dda75c98bfae7f42b98d63f64d2344da469e011
Rodolphe BIGOT <7094623+loperamidetv@users.noreply.github.com>
2022-11-21 20:49:59 +0400

    CRU methods working

--- mvc/models/tournament.py
+++ mvc/models/tournament.py
@@ -12,18 +12,18 @@ class Tournament(object):
     ID: int = None
     name: str = None
     location: str = None
-    date_start: datetime = None
-    date_end: datetime = None
-    number_of_rounds: int = 4
-    time_mode: str = None
-    description: str = None
+    # date_start: datetime = None
+    # date_end: datetime = None
+    # number_of_rounds: int = 4
+    # time_mode: str = None
+    # description: str = None
     
     '''
     Complexe attributes
 
     '''
-    rounds: list = None
-    players: list = None
+    # rounds: list = None
+    # players: list = None
     
 
     '''
@@ -32,19 +32,50 @@ class Tournament(object):
     '''
 
     def create(self) -> None:
+        '''
+        Pretty straighforward method
+
+        And I'll regret writing that later... 
+
+        '''
         serialized_data = self.__dict__
         serialized_data.pop('ID')
 
-        TOURNAMENTS_TABLE.insert(serialized_data)
-
+        self.ID = TOURNAMENTS_TABLE.insert(serialized_data)
+        
         # we could eventually return the previous line in order to get the ID of the db entry... 
         return None
 
     def read(self) -> None:
-        pass
+        for key, value in TOURNAMENTS_TABLE.get(doc_id=self.ID).items():
+            self.__dict__[key] = value
+            
 
     def update(self) -> None:
-        pass
+        '''
+        For an unknown reason, when initialize data = self.__dict__, this modifies
+        the tournament itself, maybe it's because python itself, probably a memory pointer thing 
+        related... Didn't dig a lot on this... I love you python, but you made me lost an hour on this...
+        '''
+        
+        data = {}
+
+        '''
+        Basically, it gets all the attributes, thks to self.dict thing
+        run through it and update the data. 
+        I don't need an ID updating. (this'd be non sense...)
+        so I ditch the ID part (by if-ing...)
+        '''
+        for key, value in self.__dict__.items():
+            if key != 'ID':
+                data[key] = value
+
+
+        TOURNAMENTS_TABLE.update(data, doc_ids=[self.ID])
+
+        return None
+
+
 
     def delete(self) -> None:
         pass
